4dc5df5ce05fd44b6c070dacd7c55a3df615a4fa
Harsh Valecha <54684359+Harshvalecha31@users.noreply.github.com>
2021-10-10 17:11:01 +0530

    Student Form Views Update
    Form Rendering Correctly on my local system for new index method

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+# First Request
+# def index(request):
+# 	return HttpResponse("Hello World")
+
+# Second Request
+from .forms import StudentForm
+
 def index(request):
-	return HttpResponse("Hello World")
+	if request.method=='POST':
+		form = StudentForm(request.POST)
+		if form.is_valid:
+			form.save()
+	form = StudentForm()
+	return render(request,'index.html',{'form':form})
