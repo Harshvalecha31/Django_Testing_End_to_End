afdd6ab2a4bd6d4edfc7c8d7bc017eee207cd47c
Harsh Valecha <54684359+Harshvalecha31@users.noreply.github.com>
2021-10-10 15:09:57 +0530

    Created TestCases using SimpleTestCase class
    Validation for valid and invalid values check from response

--- firstapp/tests.py
+++ firstapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from django.test import SimpleTestCase
 # Create your tests here.
+
+
+# Creating a simple testcase in django
+class FirstTest(SimpleTestCase):
+
+	# checking valid value
+	def test_valid_value(self):
+		self.assertContains(self.client.get(''),"Hello World")
+
+	# Checking invalid value 
+	def test_invalid_value(self):
+		self.assertNotContains(self.client.get(''),"hello world")
+
